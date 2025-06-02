b26edd34e6abacbda1fb772c5ea2958d3780fac2
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-06-02 12:14:25 +0530

    Trying first edition

---
+++ NodeTLS/TlsClient.cs
@@ -0,0 +1,97 @@
+﻿using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+
+namespace NodeTLS;
+
+public class TlsClient: System.IDisposable
+{
+    private readonly string targetHostName;
+    private readonly TcpClient client;
+    private SslStream ssl;
+    private bool disposedValue;
+
+    public string ServerName => targetHostName;
+
+    public TlsClient(string targetHostName, TcpClient client, SslStream ssl)
+    {
+        this.targetHostName = targetHostName;
+        this.client = client;
+        this.ssl = ssl;
+    }
+
+    public Task ForwardToSocket(string host, int port, bool includeRemoteEndPoint = true)
+    {
+        return Task.Run(async () => {
+            try
+            {
+                using var client = new Socket(AddressFamily.Ipx, SocketType.Stream, ProtocolType.Tcp);
+                var endPoint = new IPEndPoint(IPAddress.Parse(host), port);
+                await client.ConnectAsync(endPoint);
+                using var ns = new NetworkStream(client, true);
+
+                await Task.WhenAll(this.ssl.CopyToAsync(ns), ns.CopyToAsync(ssl));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        });
+    }
+
+
+    public Task ForwardToUnixSocket(string host, string port, bool includeRemoteEndPoint = true)
+    {
+        return Task.Run(async () => {
+            try
+            {
+                using var client = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
+                var endPoint = new UnixDomainSocketEndPoint(port);
+
+                await client.ConnectAsync(endPoint);
+                using var ns = new NetworkStream(client, true);
+
+                await Task.WhenAll(this.ssl.CopyToAsync(ns), ns.CopyToAsync(ssl));
+
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        });
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                try {
+                    ssl.Close();
+                } catch {
+                }
+                try {
+                    client.Close();
+                } catch { }
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~TlsClient()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ NodeTLS/TlsListener.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace NodeTLS;
+
+public class TlsListener
+{
+
+    public static async Task<TlsListener> Create(
+        string host,
+        int port,
+        Func<string, Task<string>> sniCallback,
+        Func<TlsClient, Task> read)
+    {
+        var listener = new TlsListener(host, port, sniCallback, read);
+        await listener.CreateInternal();
+        return listener;
+    }
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    private string host;
+    private int port;
+    private Func<string, Task<string>> sniCallback;
+    private Func<TlsClient, Task> read;
+
+    public TlsListener(string host, int port, Func<string, Task<string>> sniCallback, Func<TlsClient, Task> read)
+    {
+        this.host = host;
+        this.port = port;
+        this.sniCallback = sniCallback;
+        this.read = read;
+    }
+    
+    private async Task CreateInternal()
+    {
+        var server = new TcpListener(string.IsNullOrWhiteSpace(host)
+            ? System.Net.IPAddress.None
+            : System.Net.IPAddress.Parse(host), port);
+        await Task.Run(async () => {
+            while (!cts.IsCancellationRequested)
+            {
+                try
+                {
+                    await AcceptSocket(server);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex);
+                }
+            }
+        });
+    }
+
+    private async Task AcceptSocket(TcpListener server)
+    {
+        var client = await server.AcceptTcpClientAsync();
+        try
+        {
+            using var ssl = new SslStream(client.GetStream());
+
+            await ssl.AuthenticateAsServerAsync(AuthenticateAsync, this, CancellationToken.None);
+
+            using var tlsClient = new TlsClient(ssl.TargetHostName, client, ssl);
+
+            await this.read(tlsClient);
+
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+            try
+            {
+                client.Close();
+            } catch { }
+        }
+    }
+
+    private static async ValueTask<SslServerAuthenticationOptions> AuthenticateAsync(SslStream stream, SslClientHelloInfo clientHelloInfo, object? state, CancellationToken cancellationToken)
+    {
+        var listener = (state as TlsListener)!;
+        var name = clientHelloInfo.ServerName;
+
+        var jsonNode = JsonNode.Parse(await listener.sniCallback(name))!;
+        var cert = jsonNode.AsObject()["cert"]!.AsValue().ToString();
+        var key = jsonNode.AsObject()["key"]!.AsValue().ToString();
+
+        var xCert = X509Certificate2.CreateFromPem(cert, key);
+
+        return new SslServerAuthenticationOptions
+        {
+            ServerCertificateContext = SslStreamCertificateContext.Create(xCert, null)
+
+        };
+    }
+}
